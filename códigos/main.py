7ec15b4f1e728ef450f97e1951080451665cae8d
joaohrx <victor.rocha1@escolar.ifrn.edu.br>
2025-07-31 13:20:58 -0300

    Corrigi a bomba da estrutura de arquivos

--- códigos/main.py
+++ códigos/main.py
@@ -2,21 +2,19 @@ import pygame
 from abc import ABC, abstractmethod
 pygame.init()
 
-
-#Tela
+# Tela
 x = 1280
 y = 720
 tela = pygame.display.set_mode((x, y))
 pygame.display.set_caption('ECOS')
 
-
-# Musica
-pygame.mixer.music.load('FUNDO_MUSICAL.mp3')  
+# Música
+pygame.mixer.music.load('assets/FUNDO_MUSICAL.mp3')  
 pygame.mixer.music.play(-1) 
 
-# tela de start
+# Tela de start
 def tela_inicial():
-    imagem_inicial = pygame.image.load('tela_inicial1.png')  
+    imagem_inicial = pygame.image.load('assets/Tela_inicial1.png')  
     imagem_inicial = pygame.transform.scale(imagem_inicial, (x, y))
     esperando = True
 
@@ -33,8 +31,8 @@ def tela_inicial():
 
 class Personagem(ABC):
     def __init__(self, imagem_path, tamanho, posi, velocidade):
-        self.imagem = pygame.image.load(imagem_path).convert_alpha() # definir imagem
-        self.imagem = pygame.transform.scale(self.imagem, tamanho) # transformar imagem pra o tamanho certo
+        self.imagem = pygame.image.load(imagem_path).convert_alpha() 
+        self.imagem = pygame.transform.scale(self.imagem, tamanho) 
         self.posi = list(posi)
         self.velocidade = velocidade
         self.vivo = True
@@ -49,8 +47,7 @@ class Personagem(ABC):
             
 class Protagonista(Personagem):
     def __init__(self):
-        super().__init__('Protagonista.png', (60, 60), (616, 336), 0.5)
-
+        super().__init__('assets/Protagonista.png', (60, 60), (616, 336), 0.5)
 
     def atualizar(self, teclas):
         if teclas[pygame.K_w]:
@@ -62,17 +59,15 @@ class Protagonista(Personagem):
         if teclas[pygame.K_d]:
             self.posi[0] += self.velocidade
             
-class Ceifador(Personagem): # monstro
+class Ceifador(Personagem):
     def __init__(self):
-        super().__init__('monstro.png', (200, 200), (-200, 200), 0.7)
+        super().__init__('assets/monstro.png', (200, 200), (-200, 200), 0.7)
         
     def atualizar(self):
-        self.posi [0] += self.velocidade
-        
+        self.posi[0] += self.velocidade
 
-
-#Carregar plano de fundo
-fundo = pygame.image.load('image.11.png').convert_alpha()
+# Carregar plano de fundo
+fundo = pygame.image.load('assets/image.11.png').convert_alpha()
 fundo = pygame.transform.scale(fundo, (x, y))
 
 protagonista = Protagonista()
@@ -84,7 +79,7 @@ protagonista_vivo = True
 
 tela_inicial()
 
-#inicializar
+# Loop principal
 rodando = True
 while rodando:
     for event in pygame.event.get():
@@ -93,8 +88,7 @@ while rodando:
 
     tela.blit(fundo, (0, 0))
 
-
-# desenhar o monstro e aplicar o limite de respawn
+    # Monstro e respawn
     if monstros_mortos < max_monstros and monstro.vivo:
         monstro.atualizar()
         monstro.desenhar(tela)
@@ -103,15 +97,15 @@ while rodando:
             monstros_mortos += 1
             monstro.vivo = False  
     elif monstros_mortos < max_monstros:
-         monstro = Ceifador()
+        monstro = Ceifador()
 
-    # atualizar e desenhar o protagonista
+    # Protagonista
     teclas = pygame.key.get_pressed()
     protagonista.atualizar(teclas)
     protagonista.desenhar(tela)
 
     pygame.display.update()
-    
+
 
 
 
