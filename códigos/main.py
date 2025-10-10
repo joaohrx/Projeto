f6ca9075291058ecef4e66cdeedacf2e3e458e3b
joaohrx <victor.rocha1@escolar.ifrn.edu.br>
2025-10-10 14:27:57 -0300

    Transição na tela inicial e troca de cenário inicial

--- códigos/main.py
+++ códigos/main.py
@@ -13,14 +13,41 @@ pygame.mixer.music.load('assets/FUNDO_MUSICAL.mp3')
 pygame.mixer.music.play(-1)
 
 # Fundo
-fundo = pygame.image.load('assets/image.11.png').convert_alpha()
-fundo = pygame.transform.scale(fundo, (X, Y))
+fundo_menu = pygame.image.load('assets/telainicial.png').convert()
+fundo_menu = pygame.transform.scale(fundo_menu, (X, Y))
+
+fundo_jogo = pygame.image.load('assets/telainicial.png').convert()
+fundo_jogo = pygame.transform.scale(fundo_jogo, (X, Y))
 
 protagonista = Protagonista()
 monstro = Ceifador()
 monstros_mortos = 0
 max_monstros = 3
+
+
 tela_inicial(tela)
+
+def transicao_fade(tela, img_saida, img_entrada, duracao=1500):
+    clock = pygame.time.Clock()
+    fade_surface = pygame.Surface((X, Y))
+    fade_surface.fill((0, 0, 0))
+
+    for alpha in range(0, 255, 10):
+        tela.blit(img_saida, (0, 0))
+        fade_surface.set_alpha(alpha)
+        tela.blit(fade_surface, (0, 0))
+        pygame.display.flip()
+        clock.tick(FPS)
+
+    for alpha in range(255, -1, -10):
+        tela.blit(img_entrada, (0, 0))
+        fade_surface.set_alpha(alpha)
+        tela.blit(fade_surface, (0, 0))
+        pygame.display.flip()
+        clock.tick(FPS)
+
+transicao_fade(tela, fundo_menu, fundo_jogo, duracao=1500)
+
 
 rodando = True
 while rodando:
@@ -36,9 +63,8 @@ while rodando:
                 else:
                     tela = pygame.display.set_mode((X, Y))
 
-    tela.blit(fundo, (0, 0))
+    tela.blit(fundo_jogo, (0, 0))
 
-    # Monstro
     if monstros_mortos < max_monstros and monstro.vivo:
         monstro.atualizar()
         monstro.desenhar(tela)
--- códigos/personagens.py
+++ códigos/personagens.py
@@ -10,7 +10,8 @@ class Personagem(ABC):
         self.vivo = True
 
     @abstractmethod
-    def atualizar(self): pass
+    def atualizar(self): 
+        pass
 
     def desenhar(self, tela):
         if self.vivo:
@@ -21,7 +22,7 @@ class Personagem(ABC):
 
 class Protagonista(Personagem):
     def __init__(self):
-        super().__init__('assets/Protagonista.png', (60, 60), (616, 336), 0.5)
+        super().__init__('assets/Protagonista.png', (60, 60), (500, 300), 0.5)
 
     def atualizar(self, teclas):
         if teclas[pygame.K_w]: self.posi[1] -= self.velocidade
@@ -31,7 +32,7 @@ class Protagonista(Personagem):
 
 class Ceifador(Personagem):
     def __init__(self):
-        super().__init__('assets/monstro.png', (200, 200), (-200, 200), 0.7)
+        super().__init__('assets/monstro.png', (200, 200), (-300, 300), 0.7)
 
     def atualizar(self):
         self.posi[0] += self.velocidade
