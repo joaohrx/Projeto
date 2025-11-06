50739d8f9ea71a2077d7b9963e09242d536fb009
Andresasantos <andresa.s@escolar.ifrn.edu.br>
2025-11-06 11:03:51 -0300

    Demo da interação com uma estante de livros

--- códigos/main.py
+++ códigos/main.py
@@ -17,8 +17,16 @@ pygame.mixer.music.play(-1)
 fundo_menu = pygame.image.load('assets/telainicial.png').convert()
 fundo_menu = pygame.transform.scale(fundo_menu, (X, Y))
 
-fundo_jogo = pygame.image.load('assets/Mapaa.png').convert()
+fundo_jogo = pygame.image.load('assets/Mappa.png').convert()
 fundo_jogo = pygame.transform.scale(fundo_jogo, (X, Y))
+
+# Interação estante 1
+imagem_estante = pygame.image.load('assets/estante01.png').convert_alpha()
+imagem_estante = pygame.transform.scale(imagem_estante, (600, 400))
+mostrando_livro = False
+
+area_livro = pygame.Rect(302, 302, 90,90)  # (largura altura)
+fonte = pygame.font.Font(None, 18)
 
 protagonista = Protagonista()
 monstro = Ceifador()
@@ -62,22 +70,48 @@ while rodando:
                 else:
                     tela = pygame.display.set_mode((X, Y))
 
+            # Interação estante 1
+            elif event.key == pygame.K_l:
+              
+                if protagonista.rect.colliderect(area_livro):
+                    mostrando_livro = not mostrando_livro
+                elif mostrando_livro:
+                    mostrando_livro = False  
+
     tela.blit(fundo_jogo, (0, 0))
 
-    if monstros_mortos < max_monstros and monstro.vivo:
-        monstro.atualizar(None)
-        monstro.desenhar(tela)
-        if monstro.posi[0] > 1200:
-            monstros_mortos += 1
-            monstro.vivo = False
-    elif monstros_mortos < max_monstros:
-        monstro = Ceifador()
+  
+    if not mostrando_livro:
+        if monstros_mortos < max_monstros and monstro.vivo:
+            monstro.atualizar(None)
+            monstro.desenhar(tela)
+            if monstro.posi[0] > 1200:
+                monstros_mortos += 1
+                monstro.vivo = False
+        elif monstros_mortos < max_monstros:
+            monstro = Ceifador()
 
-    teclas = pygame.key.get_pressed()
-    protagonista.atualizar(teclas)
-    protagonista.desenhar(tela)
+        teclas = pygame.key.get_pressed()
+        protagonista.atualizar(teclas)
+        protagonista.desenhar(tela)
 
-    for c in colisoes:
-        pygame.draw.rect(tela, (255, 0, 0), c, 2)
+        for c in colisoes:
+            pygame.draw.rect(tela, (255, 0, 0), c, 1)
+
+        # Interação estante 1
+        if protagonista.rect.colliderect(area_livro):
+            texto = fonte.render("L para interagir", True, (255, 255, 255))
+            tela.blit(texto, (protagonista.posi[0], protagonista.posi[1] -20))  # texto em cima do livro
+             
+       
+    if mostrando_livro:
+        overlay = pygame.Surface((X, Y))
+        overlay.set_alpha(150)
+        overlay.fill((0, 0, 0))
+        tela.blit(overlay, (0, 0))
+
+        x_img = X // 2 - imagem_estante.get_width() // 2
+        y_img = Y // 2 - imagem_estante.get_height() // 2
+        tela.blit(imagem_estante, (x_img, y_img))
 
     pygame.display.update()
